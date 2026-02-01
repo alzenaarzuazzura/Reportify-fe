8ad58b462f7bbfd32fd36b56cbab87ddeb155543
diorizqi404 <rizqiii.dio@gmail.com>
2026-02-01 23:57:47 +0700

    feat: update API URL handling to use environment variable

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://3000.devops.my.id/reportify',
+  baseURL: import.meta.env.VITE_API_URL || 'https://3000.devops.my.id/reportify',
   headers: {
     'Content-Type': 'application/json',
   },
--- src/services/components/combo/level.ts
+++ src/services/components/combo/level.ts
@@ -1,8 +1,10 @@
 import { TLabelValue } from "@reportify/types"
 import axios from "axios"
 
+const API_URL = import.meta.env.VITE_API_URL || 'https://3000.devops.my.id/reportify'
+
 export const level = {
     getAll(): Promise<TLabelValue[]> {
-        return axios.get('https://3000.devops.my.id/reportify/levels').then(res => res.data)
+        return axios.get(`${API_URL}/levels`).then(res => res.data)
     }
 }
