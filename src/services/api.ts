8178894f6531ca3f17ea43260550073917dca21b
Alzena <znrzaaaa@gmail.com>
2026-02-26 00:13:55 +0700

    26-02-2026

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'https://3000.devops.my.id/reportify',
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000/reportify',
   headers: {
     'Content-Type': 'application/json',
   },
